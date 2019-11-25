0dff1f870a712d8cd8970c287e88ea4b09bbe11e
chchang <ookk8282@gmail.com>
2019-11-25 19:28:51 +0800

    format

--- utils/utils.h
+++ utils/utils.h
@@ -4,12 +4,8 @@
 extern "C" {
 #endif
 
-int read_png(const char* filename, unsigned char** image, unsigned* height, 
-			 unsigned* width, unsigned* channels);
-
-
-void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width, 
-			   const unsigned channels);
+int read_png(const char* filename, unsigned char** image, unsigned* height, unsigned* width, unsigned* channels);
+void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width, const unsigned channels);
 
 #ifdef __cplusplus
 }
